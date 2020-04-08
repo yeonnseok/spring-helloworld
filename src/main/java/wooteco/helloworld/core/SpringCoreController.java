61ea73cd7e1749ff07be99faabeb5e589e3ad589
gracefulBrown <sunghyun.ryu@woowahan.com>
2020-04-08 13:46:49 +0900

    feat(core): sample

--- src/main/java/wooteco/helloworld/core/SpringCoreController.java
+++ src/main/java/wooteco/helloworld/core/SpringCoreController.java
@@ -3,12 +3,15 @@ package wooteco.helloworld.core;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import wooteco.helloworld.SpringCoreService;
 
 @RestController
 public class SpringCoreController {
 
     private SpringCoreService springCoreService;
+
+    public SpringCoreController(SpringCoreService springCoreService) {
+        this.springCoreService = springCoreService;
+    }
 
     @GetMapping("/my-name")
     public String myName(@RequestParam String name) throws InterruptedException {
--- src/main/java/wooteco/helloworld/core/SpringCoreService.java
+++ src/main/java/wooteco/helloworld/core/SpringCoreService.java
@@ -1,18 +1,16 @@
-package wooteco.helloworld;
+package wooteco.helloworld.core;
 
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
 
+@Service
 public class SpringCoreService {
-    private String name;
 
     public String sayName(String name) throws InterruptedException {
-        this.name = name;
-
         doHeavyWork();
 
-        return "My name is " + this.name;
+        return "My name is " + name;
     }
 
     private void doHeavyWork() throws InterruptedException {
